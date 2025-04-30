4573b42ed58c0ae668e91a1d3e397a1d5b6a7b5f
chaaerim <cofla411@ewhain.net>
2025-04-30 16:43:18 +0900

    feat: add axios instance

---
+++ src/shared/api/api.instance.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios"
+const axiosInstance = axios.create({
+  baseURL: "/",
+  withCredentials: true,
+  headers: {
+    "content-type": "application/json; charset=utf-8",
+    "accept": "application/json",
+  },
+})
+
+export interface HttpClient extends AxiosInstance {
+  delete<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
+  get<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<T>
+  patch<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
+  post<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
+  put<T = unknown>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T>
+}
+
+export const http: HttpClient = axiosInstance
