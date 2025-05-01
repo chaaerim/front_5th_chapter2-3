f9bf5609680ebc2cb29ce021d1d91a48ddf1f1e1
chaaerim <cofla411@ewhain.net>
2025-05-02 03:21:45 +0900

    feat: vitest용 env 추가

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,20 +2,25 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import path from "node:path"
+import { loadEnv } from "vite"
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // tsconfig 의 baseUrl: 'src' 와 매치
-      "@shared": path.resolve(__dirname, "./src/shared"),
-      "@entities": path.resolve(__dirname, "./src/entities"),
-      "@features": path.resolve(__dirname, "./src/features"),
-      "@widgets": path.resolve(__dirname, "./src/widgets"),
+  return {
+    plugins: [react()],
+
+    resolve: {
+      alias: {
+        // tsconfig 의 baseUrl: 'src' 와 매치
+        "@shared": path.resolve(__dirname, "./src/shared"),
+        "@entities": path.resolve(__dirname, "./src/entities"),
+        "@features": path.resolve(__dirname, "./src/features"),
+        "@widgets": path.resolve(__dirname, "./src/widgets"),
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
+    test: {
+      globals: true,
+      environment: "jsdom",
+    },
+  }
 })
